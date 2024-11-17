736b69ad4d4e91701ac88636fa5a0f3a9e041492
Marcel <boersenanalystenapp@gmail.com>
2024-11-17 13:22:48 +0100

    TailHead Recursion
    - TailHead Recursion
    - static variable Recursion

--- Recursion/Recursion/Recursion.cpp
+++ Recursion/Recursion/Recursion.cpp
@@ -1,20 +1,32 @@
-// Recursion.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
+// Recursion
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+void tailHead(int n) {
+    if (n > 0) {
+        printf("%d ", n);   // Ausgabe: 3 2 1
+        tailHead(n - 1);
+        //printf("%d ", n);   // Ausgabe: 1 2 3
+    }
 }
 
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
+//int x = 0;  // Gleich wie static int x = 0;
+int staticVariableRecursion(int n) {
+    static int x = 0;
+    if (n > 0) {
+        x++;
+        return staticVariableRecursion(n - 1) + x;
+    }
+    return 0;
+}
 
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
+int main()
+{
+    int x = 3;
+    tailHead(x);
+
+    int r = 0;
+    r = staticVariableRecursion(5);
+    printf("\n%d", r);  // Ausgabe: 25
+    return 0;
+}
